8d5bd69db2bf678308ba6696f1335abecbcc99b2
Harsh Kudtarkar <harry@usc-guestwireless-upc-newsc17340.usc.edu>
2023-03-03 00:25:31 -0800

    refactor(datapath): mid branch design

--- src/control_unit.v
+++ src/control_unit.v
@@ -7,26 +7,24 @@ module control_unit (
   input      [6:0] opcode_i,
   input            reset_i,
   input            wb_ff_i,
-  output reg       mem_read_i, mem_to_reg_i, mem_write_i, reg_write_i, load_i, store_i, immd_i, jal_i, jalr_i, branch_i
+  output reg       mem_to_reg_i, mem_write_i, reg_write_i, load_i, store_i, immd_i, jal_i, jalr_i, branch_i
   
   );   
 
   always @(*) begin
     if (reset_i == 1'b1 || wb_ff_i) begin
-      mem_read_i = 1'b0;
-      mem_to_reg_i = 1'b0;
+      mem_to_reg_i = 1'bx;
       mem_write_i = 1'b0;
       reg_write_i = 1'b0;
-      immd_i = 1'b0;
-      load_i = 1'b0;
-      store_i = 1'b0;
+      immd_i = 1'bx;
+      load_i = 1'bx;
+      store_i = 1'bx;
       jal_i=1'b0;
       jalr_i=1'b0;
       branch_i=1'b0;
     end else begin
       case (opcode_i)
     7'b0110011: begin                // * R - Type
-          mem_read_i = 1'b0;
           mem_to_reg_i = 1'b0;
           mem_write_i = 1'b0;
           reg_write_i = 1'b1;
@@ -38,7 +36,6 @@ module control_unit (
           branch_i=1'b0;
           end
     7'b0010011: begin                // * I - Type (Arithmetic)
-          mem_read_i = 1'b0;
           mem_to_reg_i = 1'b0;
           mem_write_i = 1'b0;
           reg_write_i = 1'b1;
@@ -50,7 +47,6 @@ module control_unit (
           branch_i=1'b0;
           end
     7'b0000011: begin                // * I - Type (load)
-          mem_read_i = 1'b1;
           mem_to_reg_i = 1'b1;
           mem_write_i = 1'b0;
           reg_write_i = 1'b1;
@@ -62,7 +58,6 @@ module control_unit (
           branch_i=1'b0;
           end
     7'b0100011: begin                // * S - Type (store)
-          mem_read_i = 1'b0;
           mem_to_reg_i = 1'bx;
           mem_write_i = 1'b1;
           reg_write_i = 1'b0;
@@ -73,8 +68,7 @@ module control_unit (
           jalr_i=1'b0;
           branch_i=1'b0;
           end
-    7'b1100011: begin                // * SB - Type (branch)
-            mem_read_i = 1'b0;
+    7'b1100011: begin                // * B - Type (branch)
             mem_to_reg_i = 1'b0;
             mem_write_i = 1'b0;
             reg_write_i = 1'b0;
@@ -85,38 +79,35 @@ module control_unit (
             jalr_i=1'b0;
             branch_i=1'b1;
             end
-    7'b1101111: begin                // * UJ - Type (jal)  
-              mem_read_i = 1'b0;
+    7'b1101111: begin                // * J - Type (jal)  
               mem_to_reg_i = 1'b0;
               mem_write_i = 1'b0;
               reg_write_i = 1'b1;
-              immd_i = 1'b0;
-              load_i = 1'b0;
-              store_i = 1'b0;
+              immd_i = 1'bx;
+              load_i = 1'bx;
+              store_i = 1'bx;
               jal_i=1'b1;
               jalr_i=1'b0;
-              branch_i=1'b0;
+              branch_i=1'bx;
               end       
     7'b1100111: begin                // * I - Type (jalr)
-                mem_read_i = 1'b0;
                 mem_to_reg_i = 1'b0;
                 mem_write_i = 1'b0;
                 reg_write_i = 1'b1;
-                immd_i = 1'b0;           // it can be don't care also
-                load_i = 1'b0;
-                store_i = 1'b0;
+                immd_i = 1'b1;           // it can be don't care also
+                load_i = 1'bx;
+                store_i = 1'bx;
                 jal_i=1'b0;
                 jalr_i=1'b1;
                 branch_i=1'b0;
                 end         
     default: begin                // * nop
-          mem_read_i = 1'b0;
-          mem_to_reg_i = 1'b0;
+          mem_to_reg_i = 1'bx;
           mem_write_i = 1'b0;
           reg_write_i = 1'b0;
-          immd_i = 1'b0;
-          load_i = 1'b0;
-          store_i = 1'b0;
+          immd_i = 1'bx;
+          load_i = 1'bx;
+          store_i = 1'bx;
           jal_i=1'b0;
           jalr_i=1'b0;
           branch_i=1'b0;
